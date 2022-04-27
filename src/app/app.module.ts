2fa72a5f12a4276be9c205901e731948ebc7cb21
MulletBoy42 <96805001+MulletBoy42@users.noreply.github.com>
2022-04-27 18:03:12 -0500

    recipe app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list.import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'selector-name',
+  templateUrl: 'name.component.html'
+})
+
+export class NameComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
